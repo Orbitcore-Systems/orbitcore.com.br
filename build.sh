20b95746b84c236728dd0fe4c6a87405813358bb
JuanBindez <juanbindez780@gmail.com>
2024-11-06 15:16:20 -0300

    0.0.1 update

---
+++ build.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+VERSION=0
+MINOR=0
+PATCH=1
+EXTRAVERSION=""
+
+NOTES="update"
+BRANCH="main"
+
+if [[ -z $PATCH ]]; then
+    PATCH=""
+else
+    PATCH=".$PATCH"
+fi
+
+if [[ $EXTRAVERSION == *"-rc"* ]]; then
+    FULL_VERSION="$VERSION.$MINOR$PATCH$EXTRAVERSION"
+else
+
+    if [[ -z $EXTRAVERSION ]]; then
+        FULL_VERSION="$VERSION.$MINOR$PATCH"
+    else
+        FULL_VERSION="$VERSION.$MINOR$PATCH.$EXTRAVERSION"
+    fi
+fi
+
+git add .
+git commit -m "$FULL_VERSION $NOTES"
+git push -u origin $BRANCH
+git tag v$FULL_VERSION
+git push --tags
+
+echo "Build $FULL_VERSION completed successfully!"
